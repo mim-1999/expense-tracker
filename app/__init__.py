25a2e4fcff336589e70d09628038be06108830bc
Tasnim_mim <84861272+mim-1999@users.noreply.github.com>
2026-08-14 19:03:44 +0000

    Added a 500 server error page

--- app/__init__.py
+++ app/__init__.py
@@ -22,4 +22,8 @@ def create_app():
     def page_not_found(error):
         return render_template("404.html"), 404
 
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return render_template("500.html"), 500
+
     return app
